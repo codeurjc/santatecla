e21a355c3f4dd3711f905f67d612c05763b6aab1
Nacho <i.martinezca.2016@alumnos.urjc.es>
2019-10-19 09:30:46 +0200

    Update mermaid.js

--- front/src/assets/js/mermaid.js
+++ front/src/assets/js/mermaid.js
@@ -49204,31 +49204,10 @@ const drawClass = function (elem, classDef) {
     height: 0
   };
   const g = elem.append('g').attr('id', id).attr('class', 'classGroup');
-  const title = g.append('text').attr('x', conf.padding).attr('y', conf.textHeight + conf.padding).text(classDef.id);
-  const titleHeight = title.node().getBBox().height;
-  const membersLine = g.append('line') // text label for the x axis
-  .attr('x1', 0).attr('y1', conf.padding + titleHeight + conf.dividerMargin / 2).attr('y2', conf.padding + titleHeight + conf.dividerMargin / 2);
-  const members = g.append('text') // text label for the x axis
-  .attr('x', conf.padding).attr('y', titleHeight + conf.dividerMargin + conf.textHeight).attr('fill', 'white').attr('class', 'classText');
-  let isFirst = true;
-  classDef.members.forEach(function (member) {
-    addTspan(members, member, isFirst);
-    isFirst = false;
-  });
-  const membersBox = members.node().getBBox();
-  const methodsLine = g.append('line') // text label for the x axis
-  .attr('x1', 0).attr('y1', conf.padding + titleHeight + conf.dividerMargin + membersBox.height).attr('y2', conf.padding + titleHeight + conf.dividerMargin + membersBox.height);
-  const methods = g.append('text') // text label for the x axis
-  .attr('x', conf.padding).attr('y', titleHeight + 2 * conf.dividerMargin + membersBox.height + conf.textHeight).attr('fill', 'white').attr('class', 'classText');
-  isFirst = true;
-  classDef.methods.forEach(function (method) {
-    addTspan(methods, method, isFirst);
-    isFirst = false;
-  });
+  g.append('text').attr('x', conf.padding).attr('y', conf.textHeight + conf.padding).text(classDef.id);
   const classBox = g.node().getBBox();
   g.insert('rect', ':first-child').attr('x', 0).attr('y', 0).attr('width', classBox.width + 2 * conf.padding).attr('height', classBox.height + conf.padding + 0.5 * conf.dividerMargin);
-  membersLine.attr('x2', classBox.width + 2 * conf.padding);
-  methodsLine.attr('x2', classBox.width + 2 * conf.padding);
+
   classInfo.width = classBox.width + 2 * conf.padding;
   classInfo.height = classBox.height + conf.padding + 0.5 * conf.dividerMargin;
   idCache[id] = classInfo;
