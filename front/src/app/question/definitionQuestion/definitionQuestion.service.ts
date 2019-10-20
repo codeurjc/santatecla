d6fbcac6ede401bd13868626410e526f0ca2b569
Alejandro <alexibro2@gmail.com>
2019-10-20 20:31:55 +0200

    Refactoring front

--- front/src/app/question/definitionQuestion/definitionQuestion.service.ts
+++ front/src/app/question/definitionQuestion/definitionQuestion.service.ts
@@ -26,6 +26,26 @@ export class DefinitionQuestionService {
     return this.http.post<DefinitionAnswer>('/api/definition/question/' + id, body, {headers});
   }
 
+  addDefinitionCorrection(id: number, correct: boolean): Observable<DefinitionAnswer> {
+    if (correct) {
+      const body = JSON.stringify(correct);
+
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+      });
+
+      return this.http.post<DefinitionAnswer>('/api/definition/question/' + id + '/correct/', body, {headers});
+    } else {
+      const body = JSON.stringify(correct);
+
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+      });
+
+      return this.http.post<DefinitionAnswer>('/api/definition/question/' + id + '/wrong/', body, {headers});
+    }
+  }
+
   getDefinitionAnswers(id: number) {
     return this.http.get('/api/definition/question/' + id);
   }
--- front/src/app/question/listQuestion/listQuestion.service.ts
+++ front/src/app/question/listQuestion/listQuestion.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
-import { ListQuestion } from './listQuestion.model';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ListQuestion} from './listQuestion.model';
 
 @Injectable()
 export class ListQuestionService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   getListQuestions() {
     return this.http.get('/api/definitionList/');
   }
 
-  getListQuestion(id:number){
-    return this.http.get('api/definitionList/'+id);
+  getListQuestion(id: number) {
+    return this.http.get('api/definitionList/' + id);
   }
 
-  addListCorrection(id: number, correct: boolean):Observable<ListQuestion>{
-    if(correct){
+  ddListCorrection(id: number, correct: boolean): Observable<ListQuestion> {
+    if (correct) {
       const body = JSON.stringify(correct);
 
       const headers = new HttpHeaders({
@@ -24,9 +25,7 @@ export class ListQuestionService {
       });
 
       return this.http.post<ListQuestion>('/api/definitionList/' + id + '/correct/', body, {headers});
-    }
-    
-    else{
+    } else {
       const body = JSON.stringify(correct);
 
       const headers = new HttpHeaders({
