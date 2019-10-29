51a03d975013f563de52b9d8b597fa1f5d4c33bb
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2019-10-29 12:57:33 +0100

    Refactor view.component.ts

--- front/src/app/view/view.component.ts
+++ front/src/app/view/view.component.ts
@@ -9,40 +9,6 @@ import { RelationType } from '../relation/relation.type';
 
 declare var mermaid: any;
 
-class VisibleUnit implements Unit {
-  id: number;
-  name: string;
-  relations: VisibleRelation[] = [];
-
-  constructor(data: any) {
-    this.id = data.id;
-    this.name = data.name;
-    data.relations.forEach((relation: any) => {
-      this.relations.push(new VisibleRelation(relation));
-    });
-  }
-
-  getSortedData() {
-    return {
-      id: this.id,
-      name: this.name,
-      relations: this.relations
-    }
-  }
-}
-
-class VisibleRelation implements Relation {
-  id: number;
-  relationType: string;
-  relatedTo: any;
-
-  constructor(data: any) {
-    this.id = data.id;
-    this.relationType = data.relationType;
-    this.relatedTo = new VisibleUnit(data.relatedTo).getSortedData();
-  }
-}
-
 @Component({
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
@@ -94,6 +60,24 @@ export class ViewComponent implements OnInit, AfterContentInit {
     });
   }
 
+  private getUnit(id: number) {
+    this.viewService.getUnit(id).subscribe((data: Unit) => {
+      this.setData(data, false);
+      this.emptyResults();
+    }, error => {
+      console.log(error);
+    });
+  }
+
+
+  // Data (json)
+
+  private setData(data: any, changed: boolean) {
+    const visibleData = new VisibleUnit(data);
+    this.data = visibleData.getSortedData();
+    this.updateJson(changed);
+  }
+
   private updateJson(changed: boolean) {
     this.changed = changed;
     if (this.isValidJson()) {
@@ -120,32 +104,155 @@ export class ViewComponent implements OnInit, AfterContentInit {
     return valid;
   }
 
+  private save(event: KeyboardEvent) {
+    if (this.changed) {
+      if (event) { event.preventDefault(); }
+      if (this.isValidJson()) {
+        this.showSpinner = true;
+        this.viewService.saveUnit(this.data).subscribe((data) => {
+          this.setData(data, false);
+          this.showSpinner = false;
+        }, error => {
+          console.log(error);
+        });
+      }
+      this.changed = false;
+    }
+  }
+
+  private findUnit(data, id): VisibleUnit {
+    let unit = null;
+    if (data.id.toString() === id) {
+      unit = data;
+    } else {
+      data.relations.forEach((relation: any) => {
+        const u = this.findUnit(relation.relatedTo, id);
+        if (u !== null) {
+          unit = u;
+        }
+      });
+    }
+    return unit;
+  }
+
+
+
+  // Data (uml)
+
   private updateUml() {
     const element: any = this.umlDiv.nativeElement;
     element.innerHTML = '';
     mermaid.render('uml', this.umlParser.jsonToUml(this.data), (svgCode, bindFunctions) => {
       element.innerHTML = svgCode;
       if (this.showUmlNodeOptions) {
-        this.updateSelectedTarget();
+        this.selectedTarget = this.findUnitTarget(this.selectedTarget.id);
         this.drawUmlNodeOptions();
       }
     });
   }
 
-  private getUnit(id: number) {
-    this.viewService.getUnit(id).subscribe((data: Unit) => {
-      this.setData(data, false);
-      this.emptyResults();
-    }, error => {
-      console.log(error);
-    });
+  private updateUnitName() {
+    const selectedUnit: VisibleUnit = this.findUnit(this.data, this.selectedTarget.id);
+    selectedUnit.name = this.umlNodeOptions.nativeElement.firstChild.value;
+    this.setData(this.data, true);
+    this.setShowUmlNodeOptions(false);
   }
 
-  private setData(data: any, changed: boolean) {
-    const visibleData = new VisibleUnit(data);
-    this.data = visibleData.getSortedData();
-    this.updateJson(changed);
+  private createRelation(relationType): VisibleUnit {
+    const selectedUnit: VisibleUnit = this.findUnit(this.data, this.selectedTarget.id);
+    const newUnit: VisibleUnit = new VisibleUnit({
+      id: 0,
+      name: 'Nueva unidad',
+      relations: []
+    });
+    selectedUnit.relations.push(new VisibleRelation({
+      id: 0,
+      relationType: relationType,
+      relatedTo: newUnit
+    }));
+    this.setData(this.data, true);
+    return newUnit;
   }
+
+  private findUnitTarget(id): HTMLInputElement {
+    let found = false;
+    let target: HTMLInputElement = null;
+    this.umlDiv.nativeElement.firstChild.childNodes.forEach((childNode) => {
+      const firstChild = <HTMLInputElement>childNode.firstChild;
+      if ((!found) && (firstChild) && (firstChild.id === id.toString())) {
+        target = firstChild;
+        found = true;
+      }
+    });
+    return target;
+  }
+
+  private drawUmlNodeOptions() {
+    const input = this.umlNodeOptions.nativeElement.firstChild;
+    input.style.left = (this.selectedTarget.getBoundingClientRect().left + window.pageXOffset) + 'px';
+    input.style.top = (this.selectedTarget.getBoundingClientRect().top + window.pageYOffset) + 'px';
+    input.style.width = (this.selectedTarget.getBoundingClientRect().width) + 'px';
+    input.style.height = (this.selectedTarget.getBoundingClientRect().height) + 'px';
+    input.value = (<HTMLInputElement>this.selectedTarget.nextSibling).innerHTML;
+    input.setSelectionRange(0, input.value.length);
+    const optionsStyle = this.umlNodeOptions.nativeElement.lastChild.style;
+    optionsStyle.left = (this.selectedTarget.getBoundingClientRect().right + window.pageXOffset) + 'px';
+    optionsStyle.top = (this.selectedTarget.getBoundingClientRect().top + window.pageYOffset) + 'px';
+  }
+
+  private setShowUmlNodeOptions(showUmlNodeOptions: boolean) {
+    this.showUmlNodeOptions = showUmlNodeOptions;
+  }
+
+
+
+  // Mouse
+
+  @HostListener('document:click', ['$event'])
+  public documentClick(event: Event): void {
+    const target = <HTMLInputElement>event.target;
+
+    // Search
+    if ((target.id === 'result') || (target.id === 'unit-prefix') || (target.id === 'unit-name')) {
+      this.getUnit(this.results[this.arrowKeyLocation].id);
+    } else if ((target.attributes) && (target.attributes['class']) &&
+      ((target.attributes['class'].nodeValue === 'mat-form-field-flex') || (target.attributes['class'].nodeValue.includes('mat-form-field-outline')) ||
+        (target.attributes['class'].nodeValue === 'mat-form-field-infix') || (target.id === 'search-input'))) {
+      this.setShowResults(true);
+    } else {
+      this.setShowResults(false);
+    }
+
+    // Uml
+    if ((target.tagName === 'rect') || (target.tagName ==='text')) {
+      this.selectedTarget = target;
+      if (target.tagName === 'text') {
+        this.selectedTarget = <HTMLInputElement>target.previousElementSibling;
+      }
+      this.setShowUmlNodeOptions(true);
+      this.drawUmlNodeOptions();
+    } else if (target.tagName === 'path') {
+
+    } else if (target.id === 'create-association-button') {
+      this.selectedTarget = this.findUnitTarget(this.createRelation(RelationType.ASSOCIATION).id);
+      this.drawUmlNodeOptions();
+    } else if (target.id === 'create-aggregation-button') {
+      this.selectedTarget = this.findUnitTarget(this.createRelation(RelationType.AGGREGATION).id);
+      this.drawUmlNodeOptions();
+    } else if (target.id === 'create-composition-button') {
+      this.selectedTarget = this.findUnitTarget(this.createRelation(RelationType.COMPOSITION).id);
+      this.drawUmlNodeOptions();
+    } else if (target.id === 'create-inheritance-button') {
+      this.selectedTarget = this.findUnitTarget(this.createRelation(RelationType.INHERITANCE).id);
+      this.drawUmlNodeOptions();
+    } else if ((target.id !== 'uml-edit-input') && (target.id !== 'uml-node-options')) {
+      this.setShowUmlNodeOptions(false);
+    }
+  }
+
+
+
+  //Search
 
   private search() {
     if (this.validSearchField()) {
@@ -212,146 +319,46 @@ export class ViewComponent implements OnInit, AfterContentInit {
     return completeName.split(this.UNIT_NAME_SEPARATOR)[completeName.split(this.UNIT_NAME_SEPARATOR).length - 1];
   }
 
-  private save(event: KeyboardEvent) {
-    if (this.changed) {
-      if (event) { event.preventDefault(); }
-      if (this.isValidJson()) {
-        this.showSpinner = true;
-        this.viewService.saveUnit(this.data).subscribe((data) => {
-          this.setData(data, false);
-          this.showSpinner = false;
-        }, error => {
-          console.log(error);
-        });
-      }
-      this.changed = false;
-    }
-  }
 
-  private updateUnitName() {
-    const selectedUnit: VisibleUnit = this.findUnit(this.data, this.selectedTarget.id);
-    selectedUnit.name = this.umlNodeOptions.nativeElement.firstChild.value;
-    this.setData(this.data, true);
-    this.setShowUmlNodeOptions(false);
-  }
 
-  private findUnit(data, id): VisibleUnit {
-    let unit = null;
-    if (data.id.toString() === id) {
-      unit = data;
-    } else {
-      data.relations.forEach((relation: any) => {
-        const u = this.findUnit(relation.relatedTo, id);
-        if (u !== null) {
-          unit = u;
-        }
-      });
-    }
-    return unit;
-  }
-
-  private createRelation(relationType): VisibleUnit {
-    const selectedUnit: VisibleUnit = this.findUnit(this.data, this.selectedTarget.id);
-    const newUnit: VisibleUnit = new VisibleUnit({
-      id: 0,
-      name: 'Nueva unidad',
-      relations: []
-    });
-    selectedUnit.relations.push(new VisibleRelation({
-      id: 0,
-      relationType: relationType,
-      relatedTo: newUnit
-    }));
-    this.setData(this.data, true);
-    return newUnit;
-  }
-
-  @HostListener('document:click', ['$event'])
-  public documentClick(event: Event): void {
-    const target = <HTMLInputElement>event.target;
-
-    // Search
-    if ((target.id === 'result') || (target.id === 'unit-prefix') || (target.id === 'unit-name')) {
-      this.getUnit(this.results[this.arrowKeyLocation].id);
-    } else if ((target.attributes) && (target.attributes['class']) &&
-      ((target.attributes['class'].nodeValue === 'mat-form-field-flex') || (target.attributes['class'].nodeValue.includes('mat-form-field-outline')) ||
-      (target.attributes['class'].nodeValue === 'mat-form-field-infix') || (target.id === 'search-input'))) {
-      this.setShowResults(true);
-    } else {
-      this.setShowResults(false);
-    }
-
-    // Uml
-    if ((target.tagName === 'rect') || (target.tagName ==='text')) {
-      this.selectedTarget = target;
-      if (target.tagName === 'text') {
-        this.selectedTarget = <HTMLInputElement>target.previousElementSibling;
-      }
-      this.setShowUmlNodeOptions(true);
-      this.drawUmlNodeOptions();
-    } else if (target.tagName === 'path') {
-
-    } else if (target.id === 'create-association-button') {
-      this.selectedTarget = this.findUnitTarget(this.createRelation(RelationType.ASSOCIATION).id);
-      this.drawUmlNodeOptions();
-    } else if (target.id === 'create-aggregation-button') {
-      this.selectedTarget = this.findUnitTarget(this.createRelation(RelationType.AGGREGATION).id);
-      this.drawUmlNodeOptions();
-    } else if (target.id === 'create-composition-button') {
-      this.selectedTarget = this.findUnitTarget(this.createRelation(RelationType.COMPOSITION).id);
-      this.drawUmlNodeOptions();
-    } else if (target.id === 'create-inheritance-button') {
-      this.selectedTarget = this.findUnitTarget(this.createRelation(RelationType.INHERITANCE).id);
-      this.drawUmlNodeOptions();
-    } else if ((target.id !== 'uml-edit-input') && (target.id !== 'uml-node-options')) {
-      this.setShowUmlNodeOptions(false);
-    }
-  }
-
-  private findUnitTarget(id): HTMLInputElement {
-    let found = false;
-    let target: HTMLInputElement = null;
-    this.selectedTarget.parentElement.parentElement.childNodes.forEach((childNode) => {
-      const firstChild = <HTMLInputElement>childNode.firstChild;
-      if ((!found) && (firstChild) && (firstChild.id === id.toString())) {
-        target = firstChild;
-        found = true;
-      }
-    });
-    return target;
-  }
-
-  private drawUmlNodeOptions() {
-    const input = this.umlNodeOptions.nativeElement.firstChild;
-    input.style.left = (this.selectedTarget.getBoundingClientRect().left + window.pageXOffset) + 'px';
-    input.style.top = (this.selectedTarget.getBoundingClientRect().top + window.pageYOffset) + 'px';
-    input.style.width = (this.selectedTarget.getBoundingClientRect().width) + 'px';
-    input.style.height = (this.selectedTarget.getBoundingClientRect().height) + 'px';
-    input.value = (<HTMLInputElement>this.selectedTarget.nextSibling).innerHTML;
-    input.setSelectionRange(0, input.value.length);
-    const optionsStyle = this.umlNodeOptions.nativeElement.lastChild.style;
-    optionsStyle.left = (this.selectedTarget.getBoundingClientRect().right + window.pageXOffset) + 'px';
-    optionsStyle.top = (this.selectedTarget.getBoundingClientRect().top + window.pageYOffset) + 'px';
-  }
-
-  private updateSelectedTarget() {
-    const id = this.selectedTarget.id;
-    let found = false;
-    this.umlDiv.nativeElement.firstChild.childNodes.forEach((childNode: HTMLInputElement) => {
-      const firstChild = <HTMLInputElement> childNode.firstChild;
-      if ((!found) && (childNode.firstChild) && (firstChild.id === id)) {
-        this.selectedTarget = firstChild;
-        found = true;
-      }
-    });
-  }
-
-  private setShowUmlNodeOptions(showUmlNodeOptions: boolean) {
-    this.showUmlNodeOptions = showUmlNodeOptions;
-  }
+  // Routing
 
   private goToUnit(id) {
     this.router.navigate(['/units/' + id + '/cards']);
   }
 
+}
+
+class VisibleUnit implements Unit {
+  id: number;
+  name: string;
+  relations: VisibleRelation[] = [];
+
+  constructor(data: any) {
+    this.id = data.id;
+    this.name = data.name;
+    data.relations.forEach((relation: any) => {
+      this.relations.push(new VisibleRelation(relation));
+    });
+  }
+
+  getSortedData() {
+    return {
+      id: this.id,
+      name: this.name,
+      relations: this.relations
+    }
+  }
+}
+
+class VisibleRelation implements Relation {
+  id: number;
+  relationType: string;
+  relatedTo: any;
+
+  constructor(data: any) {
+    this.id = data.id;
+    this.relationType = data.relationType;
+    this.relatedTo = new VisibleUnit(data.relatedTo).getSortedData();
+  }
 }
