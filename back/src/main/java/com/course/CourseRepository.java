0091c619b8473c3a2fff685223f466dce629dec1
JesusBorrero <shyfu98@gmail.com>
2019-11-27 18:33:34 +0100

    New Tables Hotfix

--- back/src/main/java/com/course/CourseRepository.java
+++ back/src/main/java/com/course/CourseRepository.java
@@ -12,56 +12,56 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     public List<Long> findUserCourses(long userId);
 
     @Query(value = "select COUNT(course.id) from course join course_units on course.id = course_units.course_id join unit on course_units.units_id = unit.id join unit_test_questions" +
-            " on unit.id = unit_test_questions.unit_id join test_question on unit_test_questions.test_questions_id = test_question.id " +
-            "join question_test_answers on test_question.id = question_test_answers.test_question_id join test_answer on question_test_answers.test_answers_id = test_answer.id" +
+            " on unit.id = unit_test_questions.unit_id join question on unit_test_questions.test_questions_id = question.id " +
+            "join question_test_answers on question.id = question_test_answers.test_question_id join test_answer on question_test_answers.test_answers_id = test_answer.id" +
             " join user on test_answer.user_id = user.id where course.id = ?1 and unit_test_questions.unit_id = ?2 and user.id = ?3 and test_answer.unit_id = ?2 and test_answer.correct=true ", nativeQuery = true)
     Integer findUserCorrectTestAnswers(Long courseId, Long unitId, Long userId);
 
     @Query(value = "select COUNT(course.id) from course join course_units on course.id = course_units.course_id join unit on course_units.units_id = unit.id join unit_list_questions" +
-            " on unit.id = unit_list_questions.unit_id join list_question on unit_list_questions.list_questions_id = list_question.id " +
-            "join question_list_answers on list_question.id = question_list_answers.list_question_id join list_answer on question_list_answers.list_answers_id = list_answer.id" +
+            " on unit.id = unit_list_questions.unit_id join question on unit_list_questions.list_questions_id = question.id " +
+            "join question_list_answers on question.id = question_list_answers.list_question_id join list_answer on question_list_answers.list_answers_id = list_answer.id" +
             " join user on list_answer.user_id = user.id where course.id = ?1 and unit_list_questions.unit_id = ?2 and user.id = ?3 and list_answer.unit_id = ?2 and list_answer.correct=true ", nativeQuery = true)
     Integer findUserCorrectListAnswers(Long courseId, Long unitId, Long userId);
 
     @Query(value = "select COUNT(course.id) from course join course_units on course.id = course_units.course_id join unit on course_units.units_id = unit.id join unit_definition_questions" +
-            " on unit.id = unit_definition_questions.unit_id join definition_question on unit_definition_questions.definition_questions_id = definition_question.id " +
-            "join question_definition_answers on definition_question.id = question_definition_answers.definition_question_id join definition_answer on question_definition_answers.definition_answers_id = definition_answer.id" +
+            " on unit.id = unit_definition_questions.unit_id join question on unit_definition_questions.definition_questions_id = question.id " +
+            "join question_definition_answers on question.id = question_definition_answers.definition_question_id join definition_answer on question_definition_answers.definition_answers_id = definition_answer.id" +
             " join user on definition_answer.user_id = user.id where course.id = ?1 and unit_definition_questions.unit_id = ?2 and user.id = ?3 and definition_answer.unit_id = ?2 and definition_answer.correct=true ", nativeQuery = true)
     Integer findUserCorrectDefinitionAnswers(Long courseId, Long unitId, Long userId);
 
     @Query(value = "select COUNT(course.id) from course join course_units on course.id = course_units.course_id join unit on course_units.units_id = unit.id join unit_test_questions" +
-            " on unit.id = unit_test_questions.unit_id join test_question on unit_test_questions.test_questions_id = test_question.id " +
-            "join question_test_answers on test_question.id = question_test_answers.test_question_id join test_answer on question_test_answers.test_answers_id = test_answer.id" +
+            " on unit.id = unit_test_questions.unit_id join question on unit_test_questions.test_questions_id = question.id " +
+            "join question_test_answers on question.id = question_test_answers.test_question_id join test_answer on question_test_answers.test_answers_id = test_answer.id" +
             " join user on test_answer.user_id = user.id where course.id = ?1 and unit_test_questions.unit_id = ?2 and user.id = ?3 and test_answer.unit_id = ?2", nativeQuery = true)
     Integer findUserTestAnswers(Long courseId, Long unitId, Long userId);
 
     @Query(value = "select COUNT(course.id) from course join course_units on course.id = course_units.course_id join unit on course_units.units_id = unit.id join unit_list_questions" +
-            " on unit.id = unit_list_questions.unit_id join list_question on unit_list_questions.list_questions_id = list_question.id " +
-            "join question_list_answers on list_question.id = question_list_answers.list_question_id join list_answer on question_list_answers.list_answers_id = list_answer.id" +
+            " on unit.id = unit_list_questions.unit_id join question on unit_list_questions.list_questions_id = question.id " +
+            "join question_list_answers on question.id = question_list_answers.list_question_id join list_answer on question_list_answers.list_answers_id = list_answer.id" +
             " join user on list_answer.user_id = user.id where course.id = ?1 and unit_list_questions.unit_id = ?2 and user.id = ?3 and list_answer.unit_id = ?2", nativeQuery = true)
     Integer findUserListAnswers(Long courseId, Long unitId, Long userId);
 
     @Query(value = "select COUNT(course.id) from course join course_units on course.id = course_units.course_id join unit on course_units.units_id = unit.id join unit_definition_questions" +
-            " on unit.id = unit_definition_questions.unit_id join definition_question on unit_definition_questions.definition_questions_id = definition_question.id " +
-            "join question_definition_answers on definition_question.id = question_definition_answers.definition_question_id join definition_answer on question_definition_answers.definition_answers_id = definition_answer.id" +
+            " on unit.id = unit_definition_questions.unit_id join question on unit_definition_questions.definition_questions_id = question.id " +
+            "join question_definition_answers on question.id = question_definition_answers.definition_question_id join definition_answer on question_definition_answers.definition_answers_id = definition_answer.id" +
             " join user on definition_answer.user_id = user.id where course.id = ?1 and unit_definition_questions.unit_id = ?2 and user.id = ?3 and definition_answer.unit_id = ?2", nativeQuery = true)
     Integer findUserDefinitionAnswers(Long courseId, Long unitId, Long userId);
 
     @Query(value = "select COUNT(course.id) from course join course_units on course.id = course_units.course_id join unit on course_units.units_id = unit.id join unit_definition_questions" +
-            " on unit.id = unit_definition_questions.unit_id join definition_question on unit_definition_questions.definition_questions_id = definition_question.id " +
-            "join question_definition_answers on definition_question.id = question_definition_answers.definition_question_id join definition_answer on question_definition_answers.definition_answers_id = definition_answer.id" +
+            " on unit.id = unit_definition_questions.unit_id join question on unit_definition_questions.definition_questions_id = question.id " +
+            "join question_definition_answers on question.id = question_definition_answers.definition_question_id join definition_answer on question_definition_answers.definition_answers_id = definition_answer.id" +
             " where course.id = ?1 and unit_definition_questions.unit_id = ?2 and definition_answer.unit_id = ?2 and definition_answer.correct = ?3", nativeQuery = true)
     Integer findUnitDefinitionAnswers(Long courseId, Long unitId, boolean result);
 
     @Query(value = "select COUNT(course.id) from course join course_units on course.id = course_units.course_id join unit on course_units.units_id = unit.id join unit_list_questions" +
-            " on unit.id = unit_list_questions.unit_id join list_question on unit_list_questions.list_questions_id = list_question.id " +
-            "join question_list_answers on list_question.id = question_list_answers.list_question_id join list_answer on question_list_answers.list_answers_id = list_answer.id" +
+            " on unit.id = unit_list_questions.unit_id join question on unit_list_questions.list_questions_id = question.id " +
+            "join question_list_answers on question.id = question_list_answers.list_question_id join list_answer on question_list_answers.list_answers_id = list_answer.id" +
             " where course.id = ?1 and unit_list_questions.unit_id = ?2 and list_answer.unit_id = ?2 and list_answer.correct = ?3", nativeQuery = true)
     Integer findUnitListAnswers(Long courseId, Long unitId, boolean result);
 
     @Query(value = "select COUNT(course.id) from course join course_units on course.id = course_units.course_id join unit on course_units.units_id = unit.id join unit_test_questions" +
-            " on unit.id = unit_test_questions.unit_id join test_question on unit_test_questions.test_questions_id = test_question.id " +
-            "join question_test_answers on test_question.id = question_test_answers.test_question_id join test_answer on question_test_answers.test_answers_id = test_answer.id" +
+            " on unit.id = unit_test_questions.unit_id join question on unit_test_questions.test_questions_id = question.id " +
+            "join question_test_answers on question.id = question_test_answers.test_question_id join test_answer on question_test_answers.test_answers_id = test_answer.id" +
             " where course.id = ?1 and unit_test_questions.unit_id = ?2 and test_answer.unit_id = ?2 and test_answer.correct = ?3", nativeQuery = true)
     Integer findUnitTestAnswers(Long courseId, Long unitId, boolean result);
 }
