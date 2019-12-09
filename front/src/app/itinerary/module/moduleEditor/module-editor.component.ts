2d4068d969d214dcea996610fbef9449645b9687
DavidCuencaOrtega <36362810+DavidCuencaOrtega@users.noreply.github.com>
2019-12-09 09:24:59 +0100

    Module Editor Init

--- front/src/app/itinerary/module/moduleEditor/module-editor.component.ts
+++ front/src/app/itinerary/module/moduleEditor/module-editor.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { TdDialogService } from '@covalent/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import {MatBottomSheet} from '@angular/material/bottom-sheet';
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
 
 import {Module} from '../module.model';
 
@@ -10,6 +12,7 @@ import { LoginService } from '../../../auth/login.service';
 import { UnitService } from '../../../unit/unit.service';
 import {TabService} from '../../../tab/tab.service';
 import {ModuleService} from '../module.service';
+import {Block} from '../../block.model';
 
 @Component({
   selector: 'app-module-editor',
@@ -24,6 +27,9 @@ export class ModuleEditorComponent implements OnInit {
   module: Module;
   moduleId: number;
 
+  treeControl = new NestedTreeControl<Module>(node => !!node && node.blocks);
+  dataSource = new MatTreeNestedDataSource<Module>();
+
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
               private dialogService: TdDialogService,
@@ -31,8 +37,7 @@ export class ModuleEditorComponent implements OnInit {
               private unitService: UnitService,
               private bottomSheet: MatBottomSheet,
               private tabService: TabService,
-              private moduleService: ModuleService) {
-  }
+              private moduleService: ModuleService) {}
 
   ngOnInit() {
 
@@ -41,6 +46,7 @@ export class ModuleEditorComponent implements OnInit {
       this.moduleId = params.moduleId;
       this.moduleService.getModule(this.moduleId).subscribe((module: Module) => {
         this.module = module;
+        this.dataSource.data = this.module.blocks;
         this.unitService.getUnit(this.unitId).subscribe((unit: Unit) => {
           this.tabService.setModule(unit.name, this.unitId, module.name);
         });
@@ -48,5 +54,7 @@ export class ModuleEditorComponent implements OnInit {
     });
 
   }
+
+  hasChild = (_: number, node: Module) => !!node && !!node.blocks && node.blocks.length > 0;
 
 }
