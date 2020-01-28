10b1cbca1e552e8abe233ad2989391830c062f2d
JesusBorrero <shyfu98@gmail.com>
2020-01-28 19:33:14 +0100

    Fix(Question Detail): Wrong Query Fixed

--- back/src/main/java/com/question/list/list_question/ListQuestionRepository.java
+++ back/src/main/java/com/question/list/list_question/ListQuestionRepository.java
@@ -13,7 +13,4 @@ public interface ListQuestionRepository extends JpaRepository<ListQuestion, Long
 
     @Query(value = "select list_answer.id, list_answer.correct, list_answer.user_id from question join question_list_answers on question.id = question_list_answers.list_question_id join list_answer on question_list_answers.list_answers_id = list_answer.id join user on list_answer.user_id = user.id where question.id = ?2 and user.id = ?1", nativeQuery = true)
     List<Object> findUserAnswers(long userId, long questionId);
-
-    @Query(value = "select question.question_text, count (question.id) from question join question_list_answers on question.id = question_list_answers.list_question_id join list_answer on question_list_answers.list_answers_id = list_answer.id where question.id = ?1 group by list_answer.answer", nativeQuery = true)
-    List<Object> findChosenAnswers(long questionId);
 }
