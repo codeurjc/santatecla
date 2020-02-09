d7a95dfc33f01fddf7ef65018dac4b9b70f11f36
DavidCuencaOrtega <davidcuencaortega@gmail.com>
2020-02-09 14:45:29 +0100

    feat: cards with inserts in asciidoc

--- front/src/app/card/card.component.ts
+++ front/src/app/card/card.component.ts
@@ -9,8 +9,7 @@ import {ConfirmActionComponent} from '../confirmAction/confirm-action.component'
 import Asciidoctor from 'asciidoctor';
 import {ImageComponent} from '../images/image.component';
 import {MatBottomSheet} from '@angular/material/bottom-sheet';
-import {ImageService} from "../images/image.service";
-import {Slide} from "../slide/slide.model";
+import {ImageService} from '../images/image.service';
 
 function convertToHTML(text) {
   const asciidoctor = Asciidoctor();
@@ -32,6 +31,8 @@ export class CardComponent implements OnInit, AfterViewChecked {
   cards: Card[] = [];
   unit: Unit;
   showSpinner = false;
+
+  contentCount: number;
 
   cardsView: boolean[];
   prettyCards: any[];
@@ -57,10 +58,12 @@ export class CardComponent implements OnInit, AfterViewChecked {
       this.unitService.getUnit(this.unitId).subscribe((data: Unit) => {
         this.unit = data;
         this.cards = this.unit.cards;
-        this.cards.forEach((card) => {
+        this.cards.forEach((card, index) => {
           this.cardsView.push(true);
-          this.prettyCards.push(convertToHTML(card.content));
+          this.prettyCards.push('');
+          this.cardContentProcessor(card.content, index);
         });
+        this.showSpinner = false;
       });
     });
   }
@@ -70,6 +73,82 @@ export class CardComponent implements OnInit, AfterViewChecked {
       try {
         this.fitContent(card.childNodes[2].firstChild.firstChild.childNodes[2].firstChild);
       } catch (e) {}
+    });
+  }
+
+  cardContentProcessor(content: string, index: number) {
+    this.contentCounterFunction(content);
+    let extractedData = [];
+    let position = [];
+    let counter = 0;
+    let contentCounter = 0;
+    const lineas = content.split('\n');
+    lineas.forEach((line) => {
+      const words = line.split('.');
+      if (words[0] === 'insert') {
+        const parameters = words[1].split('/');
+        if (parameters[0] === 'image') {
+          position.push(counter);
+          this.getEmbebedContent(Number(parameters[1]), content, contentCounter, 'image', extractedData, position, index);
+          contentCounter = contentCounter + 1;
+        }
+      }
+      this.addExtractedData(content, extractedData, position, index);
+      counter = counter + 1;
+    });
+  }
+
+  addExtractedData(content: string, extractedData: any, position: any, index: number) {
+    let componentsChecker = 0;
+    let cardContentExtended = '';
+    let lines = content.split('\n');
+    for (let i = 0; i < position.length; i ++) {
+      lines[position[i]] = extractedData[i];
+    }
+    lines.forEach((line: string) => {
+      cardContentExtended = cardContentExtended + line + '\n';
+    });
+    extractedData.forEach((component: string) => {
+      if (component !== '') {
+        componentsChecker = componentsChecker + 1;
+      }
+    });
+    if (componentsChecker === this.contentCount) {
+      this.showSpinner = false;
+      this.prettyCards[index] = convertToHTML(cardContentExtended);
+    }
+  }
+
+  async getEmbebedContent(content1: any, content: string, contentCounter: number, type: string, extractedData: any, position: any, index: number) {
+    let contentEmbebed;
+
+    if (type === 'image') {
+      contentEmbebed = await this.imageService.getImage(content1).toPromise().catch((error) => console.log(error));
+      if (typeof contentEmbebed !== 'undefined') {
+        const image = this.convertImage(contentEmbebed.image);
+        const img = '++++\n' +
+          '<img class="img-lesson" src="' + image + '">\n' +
+          '++++\n' +
+          '\n';
+        extractedData.splice(contentCounter, 1, img);
+      } else {
+        extractedData.splice(contentCounter, 1, 'ERROR\n');
+      }
+    }
+
+    this.addExtractedData(content, extractedData, position, index);
+  }
+
+  contentCounterFunction(content: string) {
+    this.contentCount = 0;
+    let lines: string[];
+    lines = content.split('\n');
+    lines.forEach((line: string) => {
+      let words: string[];
+      words = line.split('.');
+      if (words[0] === 'insert') {
+        this.contentCount = this.contentCount + 1;
+      }
     });
   }
 
@@ -145,12 +224,12 @@ export class CardComponent implements OnInit, AfterViewChecked {
       if (card.id === 0) {
         this.cardService.create(this.unitId, card).subscribe((createdCard) => {
           card.id = createdCard.id;
-          this.prettyCards[index] = convertToHTML(card.content);
+          this.cardContentProcessor(card.content, index);
           this.showSpinner = false;
         });
       } else {
         this.cardService.save(this.unitId, card).subscribe(() => {
-          this.prettyCards[index] = convertToHTML(card.content);
+          this.cardContentProcessor(card.content, index);
           this.showSpinner = false;
         }, error => {
           console.error(error);
