1d1e13ed77f8bbdf54a1e6bb49a8dcdff5061009
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2020-03-17 18:18:24 +0100

    fix: load unit children components

--- front/src/app/unit/unit.component.ts
+++ front/src/app/unit/unit.component.ts
@@ -1,10 +1,13 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Unit} from '../unit/unit.model';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UnitService} from './unit.service';
 import {TabService} from '../tab/tab.service';
 import {Tab} from '../tab/tab.model';
 import {CardComponent} from "../card/card.component";
+import {QuestionComponent} from "../question/question.component";
+import {LessonComponent} from "../itinerary/lesson/lesson.component";
+import {ModuleComponent} from "../itinerary/module/module.component";
 
 @Component({
   templateUrl: './unit.component.html',
@@ -18,6 +21,9 @@ export class UnitComponent implements OnInit {
   showMenu = true;
 
   @ViewChild('cards') cardComponent: CardComponent;
+  @ViewChild('questions') questionComponent: QuestionComponent;
+  @ViewChild('lessons') lessonComponent: LessonComponent;
+  @ViewChild('modules') moduleComponent: ModuleComponent;
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private unitService: UnitService, private tabService: TabService) {}
 
@@ -27,7 +33,10 @@ export class UnitComponent implements OnInit {
       this.unitService.getUnit(unitId).subscribe((data: Unit) => {
         this.tabService.addTab(new Tab('Unidad', unitId, data.name, unitId, null, null));
         this.unit = data;
-        this.cardComponent.ngOnInit();
+        if (this.cardComponent) { this.cardComponent.ngOnInit(); }
+        if (this.questionComponent) { this.questionComponent.ngOnInit(); }
+        if (this.lessonComponent) { this.lessonComponent.ngOnInit(); }
+        if (this.moduleComponent) { this.moduleComponent.ngOnInit(); }
       }, error => { console.log(error); });
     });
   }
