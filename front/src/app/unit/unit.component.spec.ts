cd6ea785e0bfe7dba2f48f0c81656ef6358d8fe4
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2020-05-20 12:51:10 +0200

    test: menu & unit components

---
+++ front/src/app/unit/unit.component.spec.ts
@@ -0,0 +1,83 @@
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {UnitService} from "./unit.service";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from "@angular/material/input";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatSelectModule} from "@angular/material/select";
+import {MatCardModule} from "@angular/material/card";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {LoginService} from "../auth/login.service";
+import {RouterTestingModule} from "@angular/router/testing";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatButtonModule} from "@angular/material/button";
+import {TabService} from "../tab/tab.service";
+import {UnitComponent} from "./unit.component";
+import {CardComponent} from "../card/card.component";
+import {LessonComponent} from "../itinerary/lesson/lesson.component";
+import {QuestionComponent} from "../question/question.component";
+import {ModuleComponent} from "../itinerary/module/module.component";
+import {DragDropModule} from "@angular/cdk/drag-drop";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+
+describe('View component', () => {
+
+  let component: UnitComponent;
+  let fixture: ComponentFixture<UnitComponent>;
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTooltipModule,
+        MatDialogModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatSelectModule,
+        DragDropModule,
+        MatChipsModule,
+        MatExpansionModule,
+        MatMenuModule,
+        MatTableModule,
+        MatPaginatorModule
+      ],
+      providers: [
+        TabService,
+        UnitService
+      ],
+      declarations: [
+        UnitComponent,
+        CardComponent,
+        LessonComponent,
+        QuestionComponent,
+        ModuleComponent
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UnitComponent);
+    component = fixture.componentInstance;
+
+  }));
+
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+
+});
